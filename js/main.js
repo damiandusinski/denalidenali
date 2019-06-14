449717a6c55244ad26867cc856cee4d134cb372f
Damian <dusinski01@gmail.com>
2019-06-14 09:45:21 +0200

    Fix menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-let menuBtn = document.querySelector(".menu-btn");
-let menuBar = document.querySelector(".menu-bar");
-let nav = document.querySelector("nav");
-let ul = document.querySelector(".menu");
-
-menuBtn.addEventListener("click", () => {
-  menuBar.classList.toggle("active");
-  nav.classList.toggle("active");
-  ul.classList.toggle("active");
-  console.log("hello");
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".menu-top").classList.toggle("active");
+  document.querySelector("nav.menu").classList.toggle("active");
 });
