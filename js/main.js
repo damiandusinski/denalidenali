868f1493c1396662c51661b4b135d8f33acd5c51
Damian <dusinski01@gmail.com>
2019-06-25 13:06:51 +0200

    Fight whit IE

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
+document.querySelector(".menu-btn").addEventListener("click", function() {
   document.querySelector(".menu-top").classList.toggle("active");
   document.querySelector("nav.menu").classList.toggle("active");
 });
